0807b98cbfdfb55c897126321ed521c4b5e3f5a1
Alexander Kiggen <alexander.kiggen@gmail.com>
2026-03-03 10:03:18 +0100

    feat: initial project setup met Maven en Spring Boot dependencies

---
+++ src/test/java/nl/novi/tickettracker/TicketTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.tickettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
