881e2ceb3bc9b6c2de8f4d3f59f10407495f3510
Alexander Kiggen <alexander.kiggen@gmail.com>
2026-03-03 11:08:19 +0100

    feat: Ticket entiteit en TicketType (enum) toegevoegd

---
+++ src/main/java/nl/novi/tickettracker/models/Ticket.java
@@ -0,0 +1,36 @@
+package nl.novi.tickettracker.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Integer id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private String status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private TicketType type;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+}
