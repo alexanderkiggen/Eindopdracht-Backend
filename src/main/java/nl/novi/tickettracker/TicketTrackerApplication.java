abd4278f60025445b26bf87acf3373259f6176bc
Alexander Kiggen <alexander.kiggen@gmail.com>
2026-03-05 10:43:55 +0100

    chore: application.properties omgebouwd naar application.yaml en database configuratie toegevoegd

--- src/main/java/nl/novi/tickettracker/TicketTrackerApplication.java
+++ src/main/java/nl/novi/tickettracker/TicketTrackerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketTrackerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TicketTrackerApplication.class, args);
     }
 
